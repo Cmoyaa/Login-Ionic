448d2b7e2813d43ff5493f49c6f6ae7950a8fc82
eiMeredith <81381928+eiMeredith@users.noreply.github.com>
2021-09-19 20:41:05 -0300

    Update home.page.ts

--- Asistencia/src/app/home/home.page.ts
+++ Asistencia/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,17 @@ export class HomePage {
 
   usuarioIngresado: string;
 
-  constructor(private routerA: ActivatedRoute) {
+  constructor(private routerA: ActivatedRoute, public toastController: ToastController) {
     this.usuarioIngresado = this.routerA.snapshot.paramMap.get('obtenerUsuario');
+    this.presentToast('¡Sesión iniciada correctamente!');
+  }
+
+  async presentToast(mensaje: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
