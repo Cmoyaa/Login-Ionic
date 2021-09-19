35dce5b7430ecac12c75b6dd839ecc3c624bdfb8
Cmoyaa <c.moya@duocuc.cl>
2021-09-19 01:49:22 -0300

    Creación de recuperar cuenta

--- Asistencia/src/app/app-routing.module.ts
+++ Asistencia/src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   },
+  {
+    path: 'recuperar-cuenta',
+    loadChildren: () => import('./recuperar-cuenta/recuperar-cuenta.module').then( m => m.RecuperarCuentaPageModule)
+  },
 ];
 
 @NgModule({
